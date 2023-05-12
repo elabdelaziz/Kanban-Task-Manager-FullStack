f73a525a17490831b5986931831b8b32a1fb9cb7
Abdulaziz Omar <elabdulazizomar@gmail.com>
2023-05-12 18:08:09 +0300

    initial setup

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import config from "./config";
+import routes from "./routes";
+
+const PORT = config.port || 5000;
+
+const app: express.Application = express();
+
+app.use(bodyParser.json());
+
+app.use("/api", routes);
+
+app.get("/", function (req: Request, res: Response) {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, function () {
+  console.log(`app running on => ${PORT}`);
+});
+
+export default app;
